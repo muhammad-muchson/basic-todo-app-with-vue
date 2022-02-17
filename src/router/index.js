8c8b88882e0c329a1761108d0f7825e4092f8f33
alterra-muchson <muchson@alterra.id>
2022-02-17 10:51:31 +0700

    initialitation
    todo app

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Todo from "../views/Todo.vue"
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/todo',
+    name: 'todo',
+    component: Todo
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
